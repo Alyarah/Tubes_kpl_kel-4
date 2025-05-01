2f24dc8130f59059b1880f01887e7c556477dda6
riyandabella <riyandabella561@gmail.com>
2025-05-01 15:37:06 +0700

    Fitur Login

---
+++ Tubes_kpl_kel 4/Login.cs
@@ -0,0 +1,45 @@
+﻿using Tubes_kpl_kel_4.Validators;
+
+namespace Tubes_kpl_kel_4
+{
+    public enum StatusLogin
+    {
+        BelumLogin,
+        Berhasil,
+        Gagal
+    }
+
+    public class Login
+    {
+        public StatusLogin Status { get; set; }
+        public string Nama { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public Login()
+        {
+            Status = StatusLogin.BelumLogin;
+        }
+
+        public string LoginUser(string nama, string email, string password)
+        {
+            bool validNama = Validasi.ValidasiNama(nama);
+            bool validEmail = Validasi.ValidasiEmail(email);
+            bool validPass = Validasi.ValidasiPassword(password);
+
+            if (!validNama || !validEmail || !validPass)
+            {
+                Status = StatusLogin.Gagal;
+                return "Login gagal. Nama, Email, atau Password tidak valid.";
+            }
+            else
+            {
+                Nama = nama;
+                Email = email;
+                Password = password;
+                Status = StatusLogin.Berhasil;
+                return $"Login berhasil.\nNama: {nama}\nEmail: {email}";
+            }
+        }
+    }
+}
--- Tubes_kpl_kel 4/Program.cs
+++ Tubes_kpl_kel 4/Program.cs
@@ -1,1 +1,22 @@
-Console.WriteLine("tes gabungan");
+using System;
+using Tubes_kpl_kel_4;
+
+class Program
+{
+    static void Main()
+    {
+        Login login = new Login();
+
+        Console.Write("Nama: ");
+        string nama = Console.ReadLine();
+
+        Console.Write("Email: ");
+        string email = Console.ReadLine();
+
+        Console.Write("Password: ");
+        string password = Console.ReadLine();
+
+        string hasil = login.LoginUser(nama, email, password);
+        Console.WriteLine(hasil);
+    }
+}
