23e39eca18f92c1eb463ccad096ca64fddec2c4a
riyandabella <riyandabella561@gmail.com>
2025-05-02 10:17:36 +0700

    update 3

---
+++ Tubes_kpl_kel 4/Riwayat/RiwayatReservasi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Tubes_kpl_kel_4.Reservasi;
+
+namespace Tubes_kpl_kel_4.RiwayatReservasi
+{
+    class StatusHandler
+    {
+        private readonly Dictionary<string, Action<DataReservasi>> _statusActions;
+
+        public StatusHandler()
+        {
+            _statusActions = new Dictionary<string, Action<DataReservasi>>(StringComparer.OrdinalIgnoreCase)
+            {
+                { "Berhasil", TampilkanBerhasil },
+                { "Dibatalkan", TampilkanDibatalkan }
+            };
+        }
+
+        private void TampilkanBerhasil(DataReservasi r)
+        {
+            Console.WriteLine($"[BERHASIL] {r.jReservasi.NamaUser} memesan {r.Ruangan} di {r.Tempat} pada {r.jReservasi.Tanggal:d}");
+        }
+
+        private void TampilkanDibatalkan(DataReservasi r)
+        {
+            Console.WriteLine($"[DIBATALKAN] Reservasi oleh {r.jReservasi.NamaUser} untuk {r.Ruangan} dibatalkan.");
+        }
+    }
+}
